1f55d06c08dd6c63a022af335e601feeeeb1ea02
Yuriy Syrov <syrovyuriy@yandex.ru>
2026-04-04 10:49:28 +0300

    rounting

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -11,18 +11,17 @@ function createPages(template, filename, chunks) {
 const htmlPages = [
   createPages('./src/index.html', './index.html', ['index']),
   createPages('./src/pages/articles.html', './pages/articles.html', ['index']),
+
+  createPages('./src/pages/articles/why-waste-time.html', './pages/articles/why-waste-time.html', ['index']),
+  createPages('./src/pages/articles/how-to-find-time-for-hobbies.html', './pages/articles/how-to-find-time-for-hobbies.html', ['index']),
+  createPages('./src/pages/articles/stress-free-group-project.html', './pages/articles/stress-free-group-project.html', ['index']),
+
   createPages('./src/pages/tests.html', './pages/tests.html', ['index']),
-  createPages('./src/pages/dictionary.html', './pages/dictionary.html', [
-    'index'
-  ]),
-  createPages(
-    './src/pages/articles/plants.html',
-    './pages/articles/plants.html',
-    ['index']
-  ),
-  createPages('./src/pages/tests/test1.html', './pages/tests/test1.html', [
-    'index'
-  ])
+  createPages('./src/pages/checklists.html', './pages/checklists.html', ['index']),
+  createPages('./src/pages/resources.html', './pages/resources.html', ['index']),
+  createPages('./src/pages/about.html', './pages/about.html', ['index']),
+  createPages('./src/pages/styleguide.html', './pages/styleguide.html', ['index']),
+  createPages('./src/pages/sitemap.html', './pages/sitemap.html', ['index']),
 ]
 
 module.exports = htmlPages
