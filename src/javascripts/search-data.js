14543c58ef33f45d4b7f55b933506f8cf32c2e51
Yuriy Syrov <syrovyuriy@yandex.ru>
2026-05-13 08:45:18 +0300

    search connent

--- src/javascripts/search-data.js
+++ src/javascripts/search-data.js
@@ -1,13 +1,13 @@
 import Airtable from "airtable";
 
-const token = '';
+const token = 'patzgawpx6WuJ6v5u.94ff76b605d466b67c3b751d85eaed234f6b684a8b55f485640be4e09fbec26b';
 
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
     apiKey: token
 });
 
-let base = Airtable.base('');
+let base = Airtable.base('appOSA7ZUZYFvY4wT');
 
 function getPostTeasers() {
     return new Promise((resolve, reject) => {
